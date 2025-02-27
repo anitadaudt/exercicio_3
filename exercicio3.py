a9e8c359c604d0e4b894fd567f5e420d84521d3b
anitadaudt <anitadaudt@gmail.com>
2025-02-27 18:14:55 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,29 @@
+"""
+Exercício 3
+Autor: Anita Daudt
+Data: 27/02/2025
+Versão 0.0.1
+"""
+
+# Alocação de memória: 
+
+numero_1 = 0
+numero_2 = 0
+soma = 0
+
+
+# Entrada de dados:
+
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+
+
+
+# Processamento de dados:
+
+soma = numero_1 + numero_2
+
+
+# Saída de dados: 
+
+print("A soma dos dois números é:", soma)
